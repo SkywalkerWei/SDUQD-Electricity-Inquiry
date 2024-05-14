d06c0ff0e3f3b65743dff03edb3e2dcce3f8127a
Skywalker Wei <139356427+SkywalkerWei@users.noreply.github.com>
2024-05-14 23:39:43 +0800

    Upload: UI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import ElectricityInquiry
 
 if __name__ == '__main__':
-    print(ElectricityInquiry.query('000000', '000', '000'))
+    acc=str(input("校园卡卡号：(6位数字)\n"))
+    bui=str(input("宿舍楼：(字母+数字)\n"))
+    roo=str(input("房间号：\n"))
+    print("该房间剩余电量："+ElectricityInquiry.query(acc, bui, roo)+"元")
     input()
